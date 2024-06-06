e4be9951a190c6230251d02c1d089e681b899992
danielschneider22 <danielschneider22@gmail.com>
2024-06-05 21:03:06 -0400

    client side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Component from "@/components/component/book-report";
+import dynamic from "next/dynamic";
 
 export default function Home() {
+  const ClientSideComponent = dynamic(() => import('@/components/component/book-report'), {
+    ssr: false,
+  })
   return (
 <main className="flex min-h-screen flex-col items-center justify-between p-6 sm:p-12 md:p-24 lg:p-24">
-      <Component />
+      <ClientSideComponent />
     </main>
   );
 }
